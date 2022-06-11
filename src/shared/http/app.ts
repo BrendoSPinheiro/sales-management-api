e0b3557d458e1d64e3ac89769a6f872364937c56
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2022-06-11 20:40:35 -0300

    chore: add typeorm config file and setup create connection with database on app

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -1,8 +1,9 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
-import { routes } from './routes';
 import { ApplicationError } from '@shared/errors';
+import { routes } from './routes';
+import '@shared/database';
 
 const app = express();
 
