57b55cdb2277d359eb8aa343d53ecec5fb5ad8ad
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2022-06-11 13:33:10 -0300

    feat: create error handling middleware

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -1,11 +1,26 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import { routes } from './routes';
+import { ApplicationError } from '@shared/errors';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof ApplicationError) {
+      return response
+        .status(error.statusCode)
+        .json({ name: error.name, message: error.message });
+    }
+
+    response
+      .status(500)
+      .json({ name: error.name, message: 'Internal server error' });
+  },
+);
 
 export { app };
