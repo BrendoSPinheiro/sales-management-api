66317536884d5311b514ce408562d1f8772bbace
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2022-06-11 13:31:44 -0300

    feat: add export point for error classes

---
+++ src/shared/errors/index.ts
@@ -0,0 +1,4 @@
+export { ApplicationError } from './ApplicationError';
+export { NotFoundException } from './NotFoundException';
+export { BadRequestException } from './BadRequestException';
+export { UnauthorizedException } from './UnauthorizedException';
