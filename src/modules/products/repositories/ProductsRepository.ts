f83a434601900c46e774a7526bc72c236a2f85fe
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2022-06-12 23:39:39 -0300

    feat: create findByName and createProduct methods on product repository

--- src/modules/products/repositories/ProductsRepository.ts
+++ src/modules/products/repositories/ProductsRepository.ts
@@ -1,5 +1,24 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { Product } from '../entities/Product';
+import { ICreateProductDto } from '../dtos/CreateProductDto';
 
 @EntityRepository(Product)
-export class ProductRepository extends Repository<Product> {}
+export class ProductRepository extends Repository<Product> {
+  public async findByName(name: string): Promise<Product | undefined> {
+    const product = this.findOne({
+      where: {
+        name,
+      },
+    });
+
+    return product;
+  }
+
+  public async createProduct(data: ICreateProductDto): Promise<Product> {
+    const product = this.create(data);
+
+    await this.save(product);
+
+    return product;
+  }
+}
